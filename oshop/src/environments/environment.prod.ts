c9c66c751482b82295814dcd602f2df654d90562
cheulong <searcheulong@gmail.com>
2018-03-18 03:05:18 +0700

    add nav

---
+++ oshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+  	 apiKey: "AIzaSyAm-VirovdhJw20AtRGzRE87SnN2yNIc0Y",
+    authDomain: "oshop-395e3.firebaseapp.com",
+    databaseURL: "https://oshop-395e3.firebaseio.com",
+    projectId: "oshop-395e3",
+    storageBucket: "",
+    messagingSenderId: "279459864479"
+  }
+};
