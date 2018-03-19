71d1e72da07199f1e46c4c95d6f079d4f5e7677e
cheulong <searcheulong@gmail.com>
2018-03-19 12:56:34 +0700

    add google authenticate

--- oshop/src/app/navbar/navbar.component.ts
+++ oshop/src/app/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable} from 'rxjs/Observable';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+	user$: Observable<firebase.User>;
+  constructor(private afAuth: AngularFireAuth) { 
+  	this.user=afAuth.authState;
+  }
 
   ngOnInit() {
   }
 
+  logout(){
+  	this.afAuth.auth.signOut();
+  }
 }
