ed3e42eef1f702bf6b382466d565bf1af3be4bac
cheulong <searcheulong@gmail.com>
2018-03-22 02:10:25 +0700

    show product in the home page

--- oshop/src/app/products/products.component.ts
+++ oshop/src/app/products/products.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ProductService} from './../product.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+	products$;
+  constructor(private productService:ProductService) { 
+  	this.products$=this.productService.getAll();
+  }
 
   ngOnInit() {
   }
